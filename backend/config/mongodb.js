f09a9dbd3946093609a2de4651351dfeeb4c2d1c
Ishita Raj <ishitaraj0505@gmail.com>
2024-12-27 22:14:16 +0530

    database connection

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -1,10 +1,20 @@
-import mongoose from 'mongoose'
+import mongoose from "mongoose";
+import 'dotenv/config'
 
-const connectDB= async()=> {
 
-    mongoose.connection.on(`connected`,()=>{
+
+const connectDB= async()=>{
+    
+    mongoose.connection.on('connected',()=>{
         console.log("DB Connected");
     })
-await mongoose.connect(`${process.env.MONGODB_URI}/ecommerce`)
+    await mongoose.connect("mongodb+srv://bhavik85746:pefa74Ol6F26P5dJ@cluster0.8rpbk.mongodb.net")
+
+
 }
+        
+
+
+console.log('Connection string:', process.env.MONGODB_URI);
+
 export default connectDB;
