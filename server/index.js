d4fa4c2f0f51b32bc0d96820f417b9affcafba77
bhavik <bhavik85747@gmail.com>
2025-01-14 14:59:39 +0530

    deployment fix

--- server/index.js
+++ server/index.js
@@ -5,14 +5,16 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const cors = require('cors');
 
+
+
 app.use(cors({
-  origin: ["http://localhost:5173", "http://localhost:8080"],
+  origin: ["http://localhost:5173", "http://localhost:8080" , "http://localhost:5173/chatroom"],
   methods: ["GET", "POST"]
 }));
 
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173", "http://localhost:8080"],
+    origin: ["http://localhost:5173", "http://localhost:8080" , "http://localhost:5173/chatroom"],
     methods: ["GET", "POST"]
   }
 });
