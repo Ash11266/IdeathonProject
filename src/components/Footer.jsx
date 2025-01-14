3689f9bbad0e53b3f748386d65799afcba9ceda3
Ishita Raj <ishitaraj0505@gmail.com>
2025-01-14 22:32:58 +0530

    Changes pt1

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -21,14 +21,14 @@ const Footer = () => {
       <div>
         <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
         <ul className='flex flex-col gap-1 text-gray-600'>
-            <li>+1-212-456-7890</li>
-            <li>contact@foreveryou.com</li>
+            <li>+91-73473 36055</li>
+            <li>contact@booksy.com</li>
         </ul>
       </div>
     </div>
     <div>
         <hr/>
-        <p className='py-5 text-sm text-center '>Copyright 2024 @forever.com - All Rights Reserved</p>
+        <p className='py-5 text-sm text-center '>Copyright 2024 @booksy.com - All Rights Reserved</p>
     </div>
     </div>
   )
--- src/components/HeroHi.tsx
+++ src/components/HeroHi.tsx
@@ -21,7 +21,7 @@ const HeroHighlightDemo = () => {
         }}
         className="pb-[18vw] text-2xl px-4 md:text-4xl lg:text-5xl font-bold text-neutral-700 dark:text-white max-w-4xl leading-relaxed lg:leading-snug text-center mx-auto "
       >
-        Books That Inspire, Conversations That Connect
+        Books That Inspire, Conversations That Connect,
         {" "}
         <Highlight className="text-black dark:text-white">
         Your Literary Haven Awaits!
